226928e62e7b43ce90e63978ecc3d81c30029aa3
drummer24 <martin.drumerpalka@gmail.com>
2018-10-11 14:23:38 +0200

    changed url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
     updatewiew = () => {
        //console.log(deployment.env)
-        fetch('process.env./index/greetings')
+        fetch(process.env.+'/index/greetings')
             .then(response => response)
             .then(message => {
                 
