fbbcb00053e89aa3c370cd06de7258092d71c4a5
drummer24 <martin.drumerpalka@gmail.com>
2018-10-12 16:44:38 +0200

    addet jsom print

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
             .then(message => {
                
                 
-                console.log(response.json())
+                console.log(messages.json())
                 this.setState({message: message});
             });
    
